962d93a81f96c238d4c68cc777d5d038b853dda2
Aniket <136266153+Anikerithmic@users.noreply.github.com>
2024-08-22 15:37:32 +0530

    Java Assignments Code Snippets

---
+++ Java/Assignments/CheckPositive.java
@@ -0,0 +1,11 @@
+class CheckPositive{
+	
+	public static void main(String args[]){
+	
+	int num = 2;
+	
+	if(num > 0)
+	System.out.println("Number is positive");
+	}
+
+}
---
+++ Java/Assignments/Square.java
@@ -0,0 +1,10 @@
+class Square{
+	
+	public static void main(String args[]){
+	
+	int s = 5;
+	
+		System.out.println("Area Of Square:" + s * s);
+	}
+
+}
